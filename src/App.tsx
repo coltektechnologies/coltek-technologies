1e18e6953a64d22a58e44b28e90bf97427aec0f6
profe-ssor <kyerematengcollins93@gmail.com>
2025-10-28 12:18:18 +0000

    Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+const App = () => {
+  const notifySuccess = () => toast.success('Success! Welcome to Coltek Technologies');
+  
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <button 
+        onClick={notifySuccess}
+        className="bg-blue-950 text-white px-10 py-2 rounded hover:bg-blue-700"
+      >
+        Hello
+      </button>
+      
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnHover
+      />
+    </div>
+  )
+}
+
+export default App
