8fd2fd39aaac27dc51b727dce824833be975e56f
Owusu Bonsu Frederick <owusubonsufrederick@gmail.com>
2025-11-28 02:57:54 +0000

    About & Contact Pages — Milestones, Routing, FAQ & UI Improvements (#19)
    * Contact Us: Issue
    
    Introduce Web 3 forms access key. Contact Form is now fully functional
    
    * About Us - Issue 3
    
    Updated the Milestone section of the page, adding animations and sldie timers.
    
    * Portfolio Page: Issue 1
    
    Implemented the Portfolio page with basic animations and filters along with other layout and design implementations
    
    * Portfolio: Bug fix
    
    Fixed lint error

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -5,6 +5,7 @@ import About from "../pages/About/About";
 import Contact from "../pages/Contact/Contact";
 import NotFound from "../pages/NotFound/NotFound";
 import LegalPage from "../pages/LegalPage/legal";
+import Portfolio from "../pages/Portfolio/Portfolio";
 
 
 const AppRoutes = () => {
@@ -15,6 +16,7 @@ const AppRoutes = () => {
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
           <Route path="legal" element={<LegalPage />} />
+          <Route path="portfolio" element={<Portfolio />}/>
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
